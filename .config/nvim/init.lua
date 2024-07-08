dd90d9dbd625dadbf4ee0d18117f74a41136100c
nad-garraz <22796744+nad-garraz@users.noreply.github.com>
2024-07-08 17:30:28 -0300

    new Neovim config files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,32 +1,41 @@
-require "user.impatient"
-require "user.keymaps"
-require "user.options"
-require "user.plugins"
-require "user.autocommands"
-require "user.cmp"
-require "user.telescope"
-require "user.gitsigns"
-require "user.treesitter"
-require "user.autopairs"
-require "user.comment"
-require "user.lualine"
-require "user.toggleterm"
-require "user.project"
-require "user.illuminate"
-require "user.alpha"
-require "user.lsp"
-require "user.dap"
-require "user.tildes_setup"
-require "user.rainbow-delimiters"
-require "user.colorizer"
-require "user.colorscheme"
-require "user.indentline" -- After colorscheme
-require "user.harpoon"
-require "user.nvim-tree"
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
--- my stuff
-require "user.hop"
-require "programming_autocommands"
-require "my"
-require "snippets.my_snippets"
-require "snippets.latex_snips"
+require 'plugins'
+
+require 'config.options'
+require 'config.keymaps'
+
+require 'utils.tildes'
+require 'utils.my'
+require 'snippets.latex_snips'
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- Load filetype-specific settings
+vim.cmd [[
+  augroup filetypedetect
+    au! BufRead,BufNewFile *.tex setfiletype tex
+  augroup END
+]]
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
